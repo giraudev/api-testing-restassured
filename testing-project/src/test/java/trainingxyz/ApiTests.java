6a1beb83d7d5f87f9c4e29227bdf3c83a0aeadf5
giraudev <giraudev@gmail.com>
2022-11-08 12:08:47 +0000

    first test code

--- testing-project/src/test/java/trainingxyz/ApiTests.java
+++ testing-project/src/test/java/trainingxyz/ApiTests.java
@@ -1,4 +1,22 @@
 package trainingxyz;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class ApiTests {
+
+    @Test
+    public void getCategories(){
+        String endpoint = "http://localhost:8888/api_testing/category/read.php";
+        var response = given().when().get(endpoint).then();
+    }
+
+    @Test
+    public void getProdut(){
+        String endpoint = "http://localhost:8888/api_testing/product/read_one.php";
+        var response =
+                given()
+                        .queryParam("id").when().get(endpoint).then();
+    }
 }
