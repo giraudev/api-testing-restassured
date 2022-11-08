9edbddc295e0a6f88f0fa972572a302d596ddf46
GIRAUDEV <giraudev@gmail.com>
2022-11-08 15:23:04 +0000

    Develop (#2)
    * created Json-Serialize test
    
    * assertThat
    
    * checked bodyresponse
    
    * handling bodyresponse
    
    * handling bodyresponse

---
+++ testing-project/src/main/java/models/Product.java
@@ -0,0 +1,25 @@
+package models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int category_id;
+
+    //Used for POST requests
+    public Product(String name, String description, double price, int category_id){
+        setName(name);
+        setDescription(description);
+        setPrice(price);
+        setCategory_id(category_id);
+    }
+}
--- testing-project/src/test/java/trainingxyz/ApiTests.java
+++ testing-project/src/test/java/trainingxyz/ApiTests.java
@@ -1,27 +1,83 @@
 package trainingxyz;
 
+import models.Product;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class ApiTests {
 
     @Test
     public void getCategories(){
         String endpoint = "http://localhost:8888/api_testing/category/read.php";
-        var response = given().when().get(endpoint).then();
+        var response = given().when().get(endpoint)
+                .then().assertThat().statusCode(200);
         response.log().body();
+    }
+
+    @Test
+    public void getProducts(){
+        String endpoint = "http://localhost:8888/api_testing/product/read.php";
+        given().when().get(endpoint)
+                .then().assertThat().statusCode(200).log().body();
     }
 
     @Test
     public void getProduct(){
         String endpoint = "http://localhost:8888/api_testing/product/read_one.php";
         var response =
-                given()
-                        .queryParam("id",2).
-                        when().
-                        get(endpoint).then();
+                given().queryParam("id",1002).
+                        when().get(endpoint).then().assertThat()
+                        .statusCode(200)
+                        .body("id",equalTo("1002"))
+                        .body("name", equalTo("Water Bottlle"))
+                        .body("description", equalTo("Blue water"))
+                        .body("price", equalTo("12.00")).body("category_id", equalTo("3"))
+                        .body("category_name", equalTo("Active Wear - Unisex"));
         response.log().body();
+    }
+
+    @Test
+    public void getProductWithAssert(){
+        String endpoint = "http://localhost:8888/api_testing/product/read_one.php";
+        var response =
+                given().queryParam("id",2).
+                        when().get(endpoint).then();
+        response.log().body();
+    }
+
+    @Test
+    public void getProductWithGreaterThan(){
+        String endpoint = "http://localhost:8888/api_testing/product/read.php";
+        var response =
+                given().when().get(endpoint).then()
+                        .log().body().assertThat().statusCode(200)
+                        .body("records.size()", greaterThan(4))
+                        .body("records.id", everyItem(notNullValue()))
+                        .body("records.name", everyItem(notNullValue()))
+                        .body("records.category_id", everyItem(notNullValue()))
+                        .body("records.category_name", everyItem(notNullValue()))
+                        .body("records.description", everyItem(notNullValue()))
+                        .body("records.price", everyItem(notNullValue()));
+                        //.body("records.id[0]",equalTo("1002"));
+    }
+
+    @Test
+    public void getProductHeader(){
+        String endpoint = "http://localhost:8888/api_testing/product/read.php";
+        var response =
+                given().when().get(endpoint).then()
+                        .log().headers().assertThat().statusCode(200)
+                        .headers("Content-Type", equalTo("application/json; charset=UTF-8"))
+                        .body("records.size()", greaterThan(4))
+                        .body("records.id", everyItem(notNullValue()))
+                        .body("records.name", everyItem(notNullValue()))
+                        .body("records.category_id", everyItem(notNullValue()))
+                        .body("records.category_name", everyItem(notNullValue()))
+                        .body("records.description", everyItem(notNullValue()))
+                        .body("records.price", everyItem(notNullValue()));
+        //.body("records.id[0]",equalTo("1002"));
     }
 
     @Test
@@ -56,10 +112,8 @@ public class ApiTests {
     public void deleteProductWithId(){
         String endpoint = "http://localhost:8888/api_testing/product/delete.php";
         var response =
-                given()
-                        .queryParam("id",20).
-                        when().
-                        delete(endpoint).then();
+                given().queryParam("id",20).
+                        when().delete(endpoint).then();
         response.log().body();
     }
 
@@ -72,9 +126,17 @@ public class ApiTests {
                }""";
 
         var response =
-                given().body(body).
-                        when().
-                        delete(endpoint).then();
+                given().body(body).when().delete(endpoint).then();
         response.log().body();
+    }
+
+    @Test
+    public void createSerializeProduct(){
+        String endpoint = "http://localhost:8888/api_testing/product/create.php";
+        Product product = new Product("Water Bottlle", "Blue water", 12, 3);
+
+        var response = given().body(product).post(endpoint).then();
+        response.log().body();
+
     }
 }
